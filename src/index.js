f740a2b2ac90b32df2f0c075adbbc68410da2c4d
Yegor Yefimov <egoryefim@gmail.com>
2020-10-08 21:37:48 +0300

    Добавил переключение темы

--- src/index.js
+++ src/index.js
@@ -2,12 +2,27 @@ import menuItemsTpl from '../src/templates/menu-items.hbs';
 import menu from './menu.json';
 
 const menuContainer = document.querySelector('.js-menu');
+const checkBox = document.querySelector('#theme-switch-toggle');
+const body = document.querySelector('body');
 const menuMarkup = createMenuMarkup(menu);
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+checkBox.addEventListener('change', onCheckBoxClick);
 
 menuContainer.insertAdjacentHTML('beforeend', menuMarkup);
 
-console.log(menuMarkup);
-
 function createMenuMarkup(menuItems) {
   return menuItems.map(menuItemsTpl).join('');
+}
+
+function onCheckBoxClick(event) {
+  if (event.target.checked) {
+    body.classList.add(Theme.DARK);
+  } else {
+    body.classList.remove(Theme.DARK);
+    body.classList.add(Theme.LIGHT);
+  }
 }
